0f62443cfac8634bf23a7d583c440a99d0084553
Cyb0nix <cedric.yoganathan@efrei.net>
2024-06-23 13:30:59 +0200

    CORS

--- cloud-analyzer-api/src/main.ts
+++ cloud-analyzer-api/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
+import {json, urlencoded} from "express";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,11 @@ async function bootstrap() {
       .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  app.enableCors();
+
+  app.use(json({ limit: '100mb' })); // Adjust limit as needed
+  app.use(urlencoded({ extended: true, limit: '100mb' }));
 
   await app.listen(3000);
 }
